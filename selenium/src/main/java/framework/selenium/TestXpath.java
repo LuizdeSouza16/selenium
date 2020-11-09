3468b108a2b737db65157bad19a8575dac4c4f21
luizdesouza16 <luizdesouza16@gmail.com>
2020-11-09 09:16:44 -0300

    xpath

--- selenium/src/main/java/framework/selenium/TestXpath.java
+++ selenium/src/main/java/framework/selenium/TestXpath.java
@@ -1,6 +1,7 @@
 package framework.selenium;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
@@ -20,9 +21,45 @@ public class TestXpath {
 		
 		driver.get("https://magazineluiza.com.br");
 		
-		String xPathAbsoluto = "/html/body/div[1]/div[2]/div/div[1]/div/div[1]/header/div/div[1]/div/div/div[2]/div[2]/div[1]/input[1]";
+		String xPathAbsoluteSearch = "/html/body/div[1]/div[2]/div/div[1]/div/div[1]/header/div/div[1]/div/div/div[2]/div[2]/div/input";
 		
-		 driver.findElement(By.xpath(xPathAbsoluto)).sendKeys("celular");
+		 driver.findElement(By.xpath(xPathAbsoluteSearch)).sendKeys("celular");
+		 
+		 String pathCellphone = "/html/body/div[2]/div[3]/div[2]/div[1]/div[3]/div/div[2]/ul/li[2]/a[1]/div/img";
+		 
+		 String searchButton = "/html/body/div[1]/div[2]/div/div[1]/div/div[1]/header/div/div[1]/div/div/div[2]/div[2]/div[1]/span";
+		 driver.findElement(By.xpath(searchButton)).click();
+		 
+         JavascriptExecutor js = (JavascriptExecutor) driver;
+		 
+		 js.executeScript("window.scrollBy(0,400)");
+		 
+		 driver.findElement(By.xpath(pathCellphone)).click();
 	
 	}
+	
+	public static void xPathRelative() {
+		System.setProperty("webdriver.edge.driver", "c:/autodrivers/msedgedriver.exe");
+		//Set driver
+		driver = new EdgeDriver();
+		//maximize the window
+		driver.manage().window().maximize();
+		
+		driver.get("https://magazineluiza.com.br");
+		
+		String inputSearch = "//input[@id='inpHeaderSearch']";
+				
+		driver.findElement(By.xpath(inputSearch)).sendKeys("celular");
+		
+		String buttonSearchString = "//span[contains(@title,'Buscar')]";
+		
+		driver.findElement(By.xpath(buttonSearchString)).click();
+		
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+			 
+		js.executeScript("window.scrollBy(0,400)");
+		
+		driver.findElement(By.xpath("//*[contains(@alt, 'Smartphone Samsung Galaxy A21s')]")).click();
+		
+		}
 }
